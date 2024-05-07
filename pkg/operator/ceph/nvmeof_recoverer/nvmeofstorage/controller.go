8f4acb59764a4fea94bab40726d3bb1cbc62aff1
cheolho.kang <cheolho.kang@samsung.com>
2024-05-07 10:07:43 +0000

    nvmeof-recoverer: implement NvmeOfStorage controller setup
    This commit included the setup logic for the NvmeOfStorage controller within the Rook Ceph operator.
    - Implementing the Add function to create a new NvmeOfStorage Controller, and add it to the CR Manager
    - Setting up the controller to watch for changes to NvmeOfStorage CRD object
    
    A reconcile function will be implemented to enable fast recovery of OSD using NVMe-oF device.
    
    Signed-off-by: cheolho.kang <cheolho.kang@samsung.com>

---
+++ pkg/operator/ceph/nvmeof_recoverer/nvmeofstorage/controller.go
@@ -0,0 +1,118 @@
+/*
+Copyright 2016 The Rook Authors. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+// Package nvmeofstorage to reconcile a NvmeOfStorage CR.
+package nvmeofstorage
+
+import (
+	"context"
+	"fmt"
+	"reflect"
+
+	"emperror.dev/errors"
+	"github.com/coreos/pkg/capnslog"
+
+	cephv1 "github.com/rook/rook/pkg/apis/ceph.rook.io/v1"
+	"github.com/rook/rook/pkg/clusterd"
+	opcontroller "github.com/rook/rook/pkg/operator/ceph/controller"
+	"github.com/rook/rook/pkg/operator/ceph/reporting"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/client-go/tools/record"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+	"sigs.k8s.io/controller-runtime/pkg/source"
+)
+
+const (
+	controllerName = "nvmeofstorage-controller"
+)
+
+var logger = capnslog.NewPackageLogger("github.com/rook/rook", controllerName)
+
+var nvmeOfStorageKind = reflect.TypeOf(cephv1.NvmeOfStorage{}).Name()
+
+// Sets the type meta for the controller main object
+var controllerTypeMeta = metav1.TypeMeta{
+	Kind:       nvmeOfStorageKind,
+	APIVersion: fmt.Sprintf("%s/%s", cephv1.CustomResourceGroup, cephv1.Version),
+}
+
+var _ reconcile.Reconciler = &ReconcileNvmeOfStorage{}
+
+// ReconcileNvmeOfStorage reconciles a NvmeOfStorage object
+type ReconcileNvmeOfStorage struct {
+	client           client.Client
+	scheme           *runtime.Scheme
+	context          *clusterd.Context
+	opManagerContext context.Context
+	recorder         record.EventRecorder
+}
+
+// Add creates a new NvmeOfStorage Controller and adds it to the Manager. The Manager will set fields on the Controller
+// and Start it when the Manager is Started.
+func Add(mgr manager.Manager, context *clusterd.Context, opManagerContext context.Context, opConfig opcontroller.OperatorConfig) error {
+	return add(mgr, newReconciler(mgr, context, opManagerContext))
+}
+
+// newReconciler returns a new reconcile.Reconciler
+func newReconciler(mgr manager.Manager, context *clusterd.Context, opManagerContext context.Context) reconcile.Reconciler {
+	return &ReconcileNvmeOfStorage{
+		client:           mgr.GetClient(),
+		context:          context,
+		scheme:           mgr.GetScheme(),
+		opManagerContext: opManagerContext,
+		recorder:         mgr.GetEventRecorderFor("rook-" + controllerName),
+	}
+}
+
+func add(mgr manager.Manager, r reconcile.Reconciler) error {
+	// Create a new controller
+	c, err := controller.New(controllerName, mgr, controller.Options{Reconciler: r})
+	if err != nil {
+		return errors.Wrapf(err, "failed to create %s controller", controllerName)
+	}
+	logger.Info("successfully started")
+
+	// Watch for changes on the NvmeOfStorage CRD object
+	cmKind := source.Kind(
+		mgr.GetCache(),
+		&cephv1.NvmeOfStorage{TypeMeta: controllerTypeMeta})
+	err = c.Watch(cmKind, &handler.EnqueueRequestForObject{}, opcontroller.WatchControllerPredicate())
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (r *ReconcileNvmeOfStorage) Reconcile(context context.Context, request reconcile.Request) (reconcile.Result, error) {
+	reconcileResponse, err := r.reconcile(request)
+	return reconcileResponse, err
+}
+
+func (r *ReconcileNvmeOfStorage) reconcile(request reconcile.Request) (reconcile.Result, error) {
+	logger.Debug("reconciling NvmeOfStorage", "Request.Namespace", request.Namespace, "Request.Name", request.Name)
+
+	// TODO (cheolho.kang): Implement the reconclie logic later
+
+	// Return and do not requeue
+	logger.Debug("done reconciling")
+	return reporting.ReportReconcileResult(logger, r.recorder, request, nil, reconcile.Result{}, nil)
+}
